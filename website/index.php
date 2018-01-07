ac3524a208a97909ca099ece0176b14b762265d2
f41007 <33663354+f41007@users.noreply.github.com>
2018-01-07 23:52:16 +0700

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/css/metro.css" rel="stylesheet">
+    <link href="css/css/metro-rtl.css" rel="stylesheet">
+    <link href="css/css/metro.min.css" rel="stylesheet">
+    <link href="css/css/metro-colors.css" rel="stylesheet">
+    <link href="css/css/metro-icons.css" rel="stylesheet">
+    <link href="css/css/metro-responsive.css" rel="stylesheet">
+    <link href="css/css/metro-schemes.css" rel="stylesheet">
+    <link href="css/css/docs.css" rel="stylesheet">
+    <link href="css/css/docs.css" rel="stylesheet">
+    <script src="css/js/metro.js"></script>
+    <script src="css/js/metro.min.js"></script>
+    <script src="css/js/jquery-2.1.3.min.js"></script>
+    <script src="css/js/jquery-3.1.0.js"></script>
+    <script src="css/js/jquery.dataTables.min.js"></script>
+    <script src="css/js/select2.min.js"></script>
+    <script src="css/js/jquery-3.1.0.min.js"></script>
+    
+    <title>Calculator Masa Depan</title>
+</head>
+<body class="op-black">
+    <header>
+<div class="app-bar op-black" data-role="appbar">
+    
+    
+    <ul class="app-bar-menu">
+        <li><a href="">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#download">Download</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</div>
+</header>
+    
+    <div class="image-container">
+                            <div class="frame op-black"><img src="images/calc.jpg"></div>
+                            <div class="image-overlay op-black">
+                                <h1 style="padding-top:200px">Kalkulator Masa Depan</h1>
+                                <p align="center">
+                                    Kalkulator Untuk Menghitung Nilai Tukar Mata Uang Digitalmu.
+                                </p>
+                            </div>
+                        </div>
+    
+    <section id="about">
+    
+    <div class="container">
+        <div align="center" style="color:white; padding-bottom:250px;">
+            <h1 style="padding-top:250px">Kalkulator Masa Depan</h1><small>About Us</small>
+            <p align="justify">
+            Kalkulator Masa Depan adalah sebuah aplikasi desktop dan mobile untuk membantu kita menyelsaikan sebuah masalah perhitungan nilai tukar mata uang digital seperti Bitcoin, karena maraknya pertukaran nilai mata uang digital pada saat ini maka kami mencoba mengembangkan aplikasi kalkulator ini. Dengan harapan dapat membantu masyarakat untuk memperhitungkan nilai mata uang digital mereka. dengan begitu kita dapat mengetahui berapa jumlah uang yang sedang kita miliki pada saat itu juga.
+            </p>
+            <button class="button cycle-button" onclick="window.location.href='#download'">&#8595;</button>
+        </div>
+    </div>
+    
+    </section>
+    
+    <section id="download">
+    <div class="image-container">
+                            <div class="frame op-black"><img src="images/blu.jpg"></div>
+                            <div class="image-overlay op-black">
+                                <h1 style="padding-top:200px">Download</h1>
+                                <p align="center">
+                                    Segera Dapatkan Kalkulator Masa Depanmu
+                                </p>
+                                <button class="button loading-pulse">Download NOW</button>
+                            </div>
+                        </div>
+    </section>
+    
+</body>
+    
+    
+</html>
